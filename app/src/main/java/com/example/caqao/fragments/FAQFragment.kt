b31f68d29c31fc3277922f43b8ff96fa54d2f5f0
fvea <fjvinceatabay@gmail.com>
2023-04-10 12:41:27 +0800

    add updates

--- app/src/main/java/com/example/caqao/fragments/FAQFragment.kt
+++ app/src/main/java/com/example/caqao/fragments/FAQFragment.kt
@@ -49,7 +49,7 @@ class FAQFragment : Fragment() {
         mList.add(
             FaqsData(
                 "How to Capture images?",
-                "eme is a cross-platform, statically typed, general-purpose programming language with type inference. Kotlin is designed to interoperate fully with Java, and the JVM version of Kotlin's standard library depends on the Java Class Library, but type inference allows its syntax to be more concise."
+                "Python is a cross-platform, statically typed, general-purpose programming language with type inference. Kotlin is designed to interoperate fully with Java, and the JVM version of Kotlin's standard library depends on the Java Class Library, but type inference allows its syntax to be more concise."
             )
         )
     }
